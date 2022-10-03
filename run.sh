ecf5bc1213d9dee120838a05c436973733b925c2
jdh5968 <jdh59681@naver.com>
2022-10-03 18:27:47 +0900

    Implement shell file that may not work properly, since I'm on Windows and don't have enough time to install linux to test this fking thing

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TITLE = "Frums - Credits EX / BGA by plaaosert / EDIT by Dohyeon / Python 3.6+"
+PY3VER = python3 --version
+
+# script
+
+clear # printf "\033c" # this would apply on most systems lul
+
+echo "Fruns - Credits EX"
+echo ""
+echo "BGA by plaaosert"
+echo "Edit by DoHyeon"
+echo ""
+echo ""
+
+sleep 3s
+
+echo "Running on Pure ${PY3VER}"
+echo ""
+echo ""
+echo ""
+
+sleep 3s
+
+echo "Starting... Please wait."
+echo ""
+echo ""
+
+sleep 3s
+
+python3 credits.py
